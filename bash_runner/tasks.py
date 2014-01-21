6735aa95a0147f76512cc97c52c0daf80510e2a3
Ran Tavory <rantav@gmail.com>
2014-01-21 16:14:39 +0200

    Initial implementation for the bash_runner. Very mocky

---
+++ bash_runner/tasks.py
@@ -0,0 +1,20 @@
+"""
+Cloudify plugin for running a simple bash script.
+
+Operations:
+    start: Run a script
+"""
+
+from celery import task
+from cosmo.events import send_event as send_riemann_event
+from cloudify.utils import get_local_ip
+
+
+get_ip = get_local_ip
+send_event = send_riemann_event
+
+
+@task
+def start(__cloudify_id, port=8080, **kwargs):
+  print 'HELLO BASH! %s' % port
+  send_event(__cloudify_id, get_ip(), "hello_bash status", "state", "running")
