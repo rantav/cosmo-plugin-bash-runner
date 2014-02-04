fda53a860992db941570d84da1c4a3f2c2120a98
Ran Tavory <rantav@gmail.com>
2014-02-04 14:18:23 +0200

    Add environment variables to the executing shell process

--- bash_runner/tasks.py
+++ bash_runner/tasks.py
@@ -18,6 +18,7 @@ from cloudify.manager import set_node_started
 get_ip = get_local_ip
 send_event = send_riemann_event
 
+
 @operation
 def start(__cloudify_id, ctx, port=8080, **kwargs):
   logger = ctx.logger
@@ -27,24 +28,28 @@ def start(__cloudify_id, ctx, port=8080, **kwargs):
   logger.info('ctx.blueprint_id=%s' % ctx.blueprint_id)
   logger.info('ctx.deployment_id=%s' % ctx.deployment_id)
   logger.info('ctx.execution_id=%s' % ctx.execution_id)
-  logger.info('ctx.properties=%s' % ctx.execution_id)
+  logger.info('ctx.properties=%s' % ctx.properties)
   # loggger.info('ctx.runtime_properties=%s' % ctx.runtime_properties)
   logger.info('get_manager_ip()=%s' % get_manager_ip())
 
-  execute("echo HELLO WORLD", ctx.logger)
+  execute('env', ctx)
 
-  start_sh = download_blueprint_file(ctx, 'start.sh')
-  bash(start_sh, logger)
+  start_sh = download_blueprint_file('start.sh', ctx)
+  bash(start_sh, ctx)
 
   set_node_started(ctx.node_id, get_ip())
 
-def bash(path, logger):
-  return execute('/bin/bash %s' % path, logger)
 
-def execute(command, logger):
-  logger.info('Running command: %s' % command)
+def bash(path, ctx):
+  return execute('/bin/bash %s' % path, ctx)
+
+
+def execute(command, ctx):
+  ctx.logger.info('Running command: %s' % command)
+  env = setup_environment(ctx)
   process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
+                             stderr=subprocess.PIPE,
+                             env=env)
   make_async(process.stdout)
   make_async(process.stderr)
 
@@ -61,9 +66,9 @@ def execute(command, logger):
     stderrPiece = read_async(process.stderr)
 
     if stdoutPiece:
-      logger.info(stdoutPiece)
+      ctx.logger.info(stdoutPiece)
     if stderrPiece:
-      logger.error(stderrPiece)
+      ctx.logger.error(stderrPiece)
 
     stdout += stdoutPiece
     stderr += stderrPiece
@@ -72,7 +77,7 @@ def execute(command, logger):
     if return_code is not None:
       break
 
-  logger.info('Done running command (return_code=%d): %s' % (return_code, command))
+  ctx.logger.info('Done running command (return_code=%d): %s' % (return_code, command))
   if (return_code == 0):
     return stdout
   else:
@@ -97,6 +102,19 @@ def read_async(fd):
       return ''
 
 
+def setup_environment(ctx):
+  '''Add some useful environment variables to the environment'''
+  env = os.environ.copy()
+  env['CLOUDIFY_NODE_ID'] = ctx.node_id
+  env['CLOUDIFY_BLUEPRINT_ID'] = ctx.blueprint_id
+  env['CLOUDIFY_DEPLOYMENT_ID'] = ctx.deployment_id
+  env['CLOUDIFY_manager_ip'] = get_manager_ip()
+  env['CLOUDIFY_EXECUTION_ID'] = ctx.execution_id
+  for k, v in ctx.properties:
+    env['CLOUDIFY_PROPERTY_%s' % k] = v
+  return env
+
+
 class ProcessException(Exception):
   def __init__(self, command, exit_code, stdout, stderr):
     Exception.__init__(self, stderr)
@@ -106,13 +124,14 @@ class ProcessException(Exception):
     self.stderr = stderr
 
 
-def download_blueprint_file(ctx, blueprint_file):
+def download_blueprint_file(blueprint_file, ctx):
   ip = get_manager_ip()
   port = 53229
   # blueprint_id = ctx.blueprint_id
   blueprint_id = '5fc21ca7-767f-47a1-8c22-8a91ff1dc02f'
   url = 'http://%s:%d/blueprint-%s/%s' % (ip, port, blueprint_id, blueprint_file)
   return download(url, ctx.logger)
+
 
 def download(http_file_path, logger):
   '''downloads a file to the local disk and returns it's disk path'''
