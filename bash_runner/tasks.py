e7de48fe5d21cb5c1d2b0ccc1ab4918503a79c48
Ran Tavory <rantav@gmail.com>
2014-02-04 11:12:45 +0200

    Adopt the plugin to the new features provided by ctx and adjust the blueprint accordingly

--- bash_runner/tasks.py
+++ bash_runner/tasks.py
@@ -5,17 +5,30 @@ Operations:
     start: Run a script
 """
 
-from celery import task
 from cosmo.events import send_event as send_riemann_event
-from cloudify.utils import get_local_ip
-
+from cloudify.utils import get_local_ip, get_manager_ip
+from cloudify.decorators import operation
+from cloudify.manager import set_node_started
 
 get_ip = get_local_ip
 send_event = send_riemann_event
 
+@operation
+def start(__cloudify_id, ctx, port=8080, **kwargs):
+  # See in context.py
+  # https://github.com/CloudifySource/cosmo-celery-common/blob/develop/cloudify/context.py
+  log(ctx, 'ctx.node_id=%s' % ctx.node_id)
+  log(ctx, 'ctx.blueprint_id=%s' % ctx.blueprint_id)
+  log(ctx, 'ctx.deployment_id=%s' % ctx.deployment_id)
+  log(ctx, 'ctx.execution_id=%s' % ctx.execution_id)
+  log(ctx, 'ctx.properties=%s' % ctx.execution_id)
+  log(ctx, 'ctx.runtime_properties=%s' % ctx.runtime_properties)
+  log(ctx, 'get_manager_ip()=%s' % get_manager_ip())
+  set_node_started(ctx.node_id, get_ip())
+  # send_event(__cloudify_id, get_ip(), "bash_runner status", "state", "running")
 
-@task
-def start(__cloudify_id, port=8080, **kwargs):
+
+def log(ctx, s):
   with open('/home/ubuntu/hello', 'w') as f:
-    print >> f, 'HELLO BASH! %s' % port
-  send_event(__cloudify_id, get_ip(), "bash_runner status", "state", "running")
+    print >> f, s
+  ctx.logger.info(s) # /var/log/celery...
