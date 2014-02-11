ced610a02708352a8584b0ff62eda5764caea514
Ran Tavory <rantav@gmail.com>
2014-02-11 12:28:34 +0200

    Install and start tty.js

--- blueprint/start.sh
+++ blueprint/start.sh
@@ -1,6 +1,25 @@
 set -e
 set -x
-
-echo START START START
 env
 
+echo Installing tty.js
+
+sudo apt-get update
+sudo apt-get install git-core curl build-essential openssl libssl-dev -y
+git clone https://github.com/joyent/node.git
+pushd node
+git checkout v0.10.25
+./configure
+make
+sudo make install
+popd
+
+node -v
+
+
+curl https://www.npmjs.org/install.sh > npm_install.sh
+sudo sh npm_install.sh
+
+sudo npm -g install tty.js
+
+tty.js --daemonize --port 8080
